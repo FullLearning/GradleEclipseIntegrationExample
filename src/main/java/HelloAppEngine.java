629f102e59a13cff972ef3d77a9696ca7a739d33
James Mortensen <james.mortensen@a-cti.com>
2018-05-01 16:12:34 +0530

    Added a greeting to the initial output in the hello servlet.

--- src/main/java/HelloAppEngine.java
+++ src/main/java/HelloAppEngine.java
@@ -31,6 +31,6 @@ public class HelloAppEngine extends HttpServlet {
 		}
 
 	
-		writer.println("Hello world James Mortensen!");
+		writer.println("Hello world James Mortensen! How are you today?");
 	}
 }
